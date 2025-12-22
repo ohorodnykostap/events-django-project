fecdb72365465bc5b89c8d549327bb734a0c0470
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2025-12-22 16:45:40 +0200

    Created an admin panel

--- event_service/admin.py
+++ event_service/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Location, EventType, Event, Booking
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("username", "email", "first_name", "last_name", "role", "is_staff")
+    list_filter = ("role", "is_staff", "is_superuser", "is_active")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        ("Role & permissions", {"fields": ("role", "is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("name", "address")
+    search_fields = ("name", "address")
+
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ("name", "event_datetime", "organizer", "location", "event_type", "status")
+    list_filter = ("status", "event_type", "location")
+    search_fields = ("name", "description", "organizer__username")
+    ordering = ("event_datetime",)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("user", "event", "status")
+    list_filter = ("status",)
+    search_fields = ("user__username", "event__name")
