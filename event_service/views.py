277e5471cb3fc5ca0470ae32a1c457d92dca3857
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2026-02-23 09:57:10 +0200

    refactor: convert booking views to CBV and fix POST handling in templates

--- event_service/views.py
+++ event_service/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Event, EventType, Location, Booking
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
 from django.utils import timezone
 from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm, EventForm, CustomUserChangeForm
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import (TemplateView,
-                                  ListView,
-                                  DetailView,
-                                  CreateView,
-                                  UpdateView)
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    TemplateView,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    View
+)
+from .models import Event, EventType, Location, Booking
+from .forms import CustomUserCreationForm, EventForm, CustomUserChangeForm
 
 
 class EventListView(ListView):
@@ -18,17 +20,6 @@ class EventListView(ListView):
     template_name = "event_service/event_list.html"
     context_object_name = "events"
     paginate_by = 6
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["event_types"] = EventType.objects.all()
-        context["locations"] = Location.objects.all()
-
-        query_params = self.request.GET.copy()
-        if "page" in query_params:
-            query_params.pop("page")
-        context["query_string"] = query_params.urlencode()
-        return context
 
     def get_queryset(self):
         queryset = Event.objects.all().order_by("event_datetime")
@@ -45,11 +36,45 @@ class EventListView(ListView):
 
         return queryset
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["event_types"] = EventType.objects.all()
+        context["locations"] = Location.objects.all()
+
+        query_params = self.request.GET.copy()
+        if "page" in query_params:
+            query_params.pop("page")
+        context["query_string"] = query_params.urlencode()
+        return context
+
 
 class EventDetailView(DetailView):
     model = Event
     template_name = "event_service/event_detail.html"
     context_object_name = "event"
+
+
+class EventCreateView(LoginRequiredMixin, CreateView):
+    model = Event
+    form_class = EventForm
+    template_name = "event_service/event_create.html"
+    success_url = reverse_lazy("events:event-list")
+    login_url = "login"
+
+    def form_valid(self, form):
+        form.instance.organizer = self.request.user
+        messages.success(self.request, "Event created successfully!")
+        return super().form_valid(form)
+
+
+class EventUpdateView(LoginRequiredMixin, UpdateView):
+    model = Event
+    form_class = EventForm
+    template_name = "event_service/event_update.html"
+    success_url = reverse_lazy("events:profile")
+
+    def get_queryset(self):
+        return Event.objects.filter(organizer=self.request.user)
 
 
 class RegisterView(CreateView):
@@ -83,65 +108,41 @@ class ProfileView(LoginRequiredMixin, TemplateView):
             form.save()
             messages.success(request, "Profile updated successfully.")
         else:
-            messages.error(request,
-                           "There was a problem updating your profile.")
+            messages.error(request, "There was a problem updating your profile.")
         return redirect("events:profile")
 
 
-class EventUpdateView(LoginRequiredMixin, UpdateView):
-    model = Event
-    form_class = EventForm
-    template_name = "event_service/event_update.html"
-    success_url = reverse_lazy("events:profile")
+class BookingCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        event = get_object_or_404(Event, pk=pk)
 
-    def get_queryset(self):
-        return Event.objects.filter(organizer=self.request.user)
+        if event.available_seats() <= 0:
+            messages.error(request, f"No available seats for '{event.name}'.")
+            return redirect("events:event-detail", pk=pk)
 
+        if Booking.objects.filter(user=request.user, event=event).exists():
+            messages.warning(request, f"You already booked '{event.name}'.")
+            return redirect("events:event-detail", pk=pk)
 
-@login_required
-def cancel_booking(request, pk):
-    booking = get_object_or_404(Booking, pk=pk, user=request.user)
-    booking.event.capacity += 1
-    booking.event.save()
-    booking.delete()
-    return redirect("events:profile")
-
-
-@login_required
-def booking_create(request, pk):
-    event = get_object_or_404(Event, pk=pk)
-
-    if event.available_seats() <= 0:
-        messages.error(request, f"No available seats for '{event.name}'.")
+        Booking.objects.create(user=request.user, event=event)
+        messages.success(request, f"Booking for '{event.name}' successful!")
         return redirect("events:event-detail", pk=pk)
 
-    if Booking.objects.filter(user=request.user, event=event).exists():
-        messages.warning(request, f"You already booked '{event.name}'.")
-        return redirect("events:event-detail", pk=pk)
 
-    Booking.objects.create(user=request.user, event=event)
-    messages.success(request, f"Booking for '{event.name}' successful!")
-    return redirect("events:event-detail", pk=pk)
-
-
-@login_required
-def booking_delete(request, pk):
-    booking = get_object_or_404(Booking, pk=pk, user=request.user)
-    event = booking.event
-    booking.delete()
-    messages.success(request,
-                     f"Booking for '{event.name}' has been cancelled.")
-    return redirect("events:profile")
+class BookingDeleteView(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        booking = get_object_or_404(Booking, pk=pk, user=request.user)
+        event = booking.event
+        booking.delete()
+        messages.success(request, f"Booking for '{event.name}' has been cancelled.")
+        return redirect("events:profile")
 
 
-class EventCreateView(LoginRequiredMixin, CreateView):
-    model = Event
-    form_class = EventForm
-    template_name = "event_service/event_create.html"
-    success_url = reverse_lazy("events:event-list")
-    login_url = "login"
-
-    def form_valid(self, form):
-        form.instance.organizer = self.request.user
-        messages.success(self.request, "Event created successfully!")
-        return super().form_valid(form)
+class BookingCancelView(LoginRequiredMixin, View):
+    def post(self, request, pk, *args, **kwargs):
+        booking = get_object_or_404(Booking, pk=pk, user=request.user)
+        booking.event.capacity += 1
+        booking.event.save()
+        booking.delete()
+        messages.success(request, f"Booking for '{booking.event.name}' cancelled.")
+        return redirect("events:profile")
